623c55323b0e33f7d58a1362b28f665b723c605e
Maxim Wilhelm <maxim-wilhelm@hotmail.com>
2022-11-15 17:37:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {  // void не возрашает данные
         System.out.println("От класса Main");
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Collections.sort(intList);          // Используется для сортировки элементов, присутствующих в указанном списке коллекции, в порядке возрастания.
+        Collections.sort(intList);           // Используется для сортировки элементов, присутствующих в указанном списке коллекции, в порядке возрастания.
         for (int x : intList) {
             if (x > 0 && x % 2 == 0)
                 System.out.println(x);
